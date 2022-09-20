4f0e44249cf6f280d981cd64a17722ccb068d369
wooohyun <woodoll543@gmail.com>
2022-09-20 16:33:00 +0900

    202209205

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+<Wrap>
+  
+</Wrap>
+  );
+}
+
+export default App;
---
+++ src/component/Header.js
@@ -0,0 +1,17 @@
+return (
+    <div className="container">
+        <h1><img src="./img/logo.png" alt="" /></h1>
+        <nav>
+            <ul>
+                <li className="Head_Nav">
+                    앱다운로드
+                </li>
+                <li>MD's추천픽</li>
+                <li>위스키</li>
+                <li>데낄라</li>
+            </ul>
+        </nav>
+    </div>
+)
+
+export default Headers;
