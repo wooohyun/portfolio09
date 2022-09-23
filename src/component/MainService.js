fb86589de4f7cd53df06f27c0c618b7de84e6961
wooohyun <woodoll543@gmail.com>
2022-09-23 14:16:34 +0900

    20220923

--- src/component/MainService.js
+++ src/component/MainService.js
@@ -1,11 +1,16 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import React, { Component } from "react";
+
 import Slider from "react-slick";
 
 const MainService = () => {
 
     const settings = {
+        vertical : true,
+        draggable : true, 
+        autoplay : true,
+        autoplaySpeed : 3000,
+        pauseOnHover : true,
         dots: true,
         infinite: true,
         speed: 1000,
@@ -18,15 +23,87 @@ const MainService = () => {
             <div className="container">
                 <Slider {...settings}>
                     <div>
-                        <figure>
-                            <img src="./img/slide01.jpg" alt="" />
-                        </figure>
+                        <div className="wrap">
+                            <div className="service">
+                                <h3>술 픽업</h3>
+                                <p>술을 주문하고 일반 식당에서 픽업하는 <br />
+                                    새로운 경험의 서비스입니다.</p>
+                                <div className="border top">
+                                    <p>전국 픽업 매장</p>
+                                    <p>1,141개</p>
+                                </div>
+                                <div className="border bottom">
+                                    <p>주문 가능한 상품</p>
+                                    <p>1,690개</p>
+                                </div>
+                            </div>
+                            <div className="mokup">
+                                <figure className="mokup_back">
+                                    <img src="./img/pickup-mockup.png" alt="" />
+                                </figure>
+                                <figure className="mokup_front">
+                                    <img src="./img/pickup-mockup_2.png" alt="" />
+                                </figure>
+                            </div>
+                        </div>
                     </div>
                     <div>
-                        <figure>
-                            <img src="./img/slide02.jpg" alt="" />
-                        </figure>
+                        <div className="wrap">
+                            <div className="service">
+                                <h3>술픽업 NOW</h3>
+                                <p>여러 리쿼샵의 다양한 상품을 <br/>
+한눈에 확인할 수 있는 서비스입니다.</p>
+                                <div className="border top">
+                                    <p>전국 픽업 매장</p>
+                                    <p>78개</p>
+                                </div>
+                                <div className="border bottom">
+                                    <p>주문 가능한 상품</p>
+                                    <p>9,914개</p>
+                                </div>
+                            </div>
+                            <div className="mokup">
+                                <figure className="mokup_back">
+                                    <img src="./img/pickup-now.png" alt="" />
+                                </figure>
+                                <figure className="mokup_front">
+                                    <img src="./img/pickup-now_2.png" alt="" />
+                                </figure>
+                            </div>
+                        </div>
                     </div>
+                    <div>
+                        <div className="wrap">
+                            <div className="service">
+                                <h3>캔맥주 멤버십</h3>
+                                <p>전국 CU 편의점에서 이용 가능한 <br/>
+캔맥주 구독 멤버십입니다.</p>
+                                <div className="border top">
+                                    <p>전국 픽업 매장</p>
+                                    <p>1,141개</p>
+                                </div>
+                                <div className="border bottom">
+                                    <p>주문 가능한 상품</p>
+                                    <p>1,690개</p>
+                                </div>
+                            </div>
+                            <div className="mokup">
+                                <figure className="mokup_back">
+                                    <img src="./img/cu-mockup.png" alt="" />
+                                </figure>
+                                <figure className="mokup_front">
+                                    <img src="./img/cu-mockup_2.png" alt="" />
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+
+                   
+
+
+
+
+
                 </Slider>
             </div>
         </mainservice>
--- src/component/MainVisual.js
+++ src/component/MainVisual.js
@@ -1,4 +1,3 @@
-import MainSlide from "./MainSlide";
 const MainVisual = () => {
     return (
         <mainvisual className="mainvisual">
@@ -8,19 +7,19 @@ const MainVisual = () => {
                 <h2>데일리샷</h2>
                 <div className="download">
                     <div className="btn">
-                        <a href="#none">
-                        <button>
-                            <img src="./img/app-store.svg" alt="" />
-                            <p>App Store</p>
-                        </button>
+                        <a target="blank" href="https://apps.apple.com/kr/app/%EB%8D%B0%EC%9D%BC%EB%A6%AC%EC%83%B7-%EC%9C%84%EC%8A%A4%ED%82%A4-%EC%99%80%EC%9D%B8-%EC%A3%BC%EB%A5%98-%EC%8A%A4%EB%A7%88%ED%8A%B8%EC%98%A4%EB%8D%94-%EC%95%B1/id1286370741">
+                            <button>
+                                <img src="./img/app-store.svg" alt="" />
+                                <p>App Store</p>
+                            </button>
                         </a>
                     </div>
                     <div className="btn">
-                    <a href="#none">
-                        <button>
-                            <img src="./img/google-play.svg" alt="" />
-                            <p>Google Play</p>
-                        </button>
+                        <a target="blank" href="https://play.google.com/store/apps/details?id=shaker.bar.com">
+                            <button>
+                                <img src="./img/google-play.svg" alt="" />
+                                <p>Google Play</p>
+                            </button>
                         </a>
                     </div>
                 </div>
